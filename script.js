4c9d8801d8963e1331bb84b7aeeb9dd28b0c3286
Shizuu <coolcoledude111@gmail.com>
2022-11-24 13:13:17 +0000

    all

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const gameBoard = (function() {
-    let gameBoardObj = {gameboard:["x","o","x","x","o","x","x","o","x"]}
-    player1 = {}
+    let gameBoardObj = {gameboard:["","","","","","","","",""]}
+    player1 = {} //factory function needed for both players
     player2 = {}
 
    
     
 
     function createGame(){
+        let counter = 0
         const gameContent = document.getElementById("container")
         for(let i = 0; i < gameBoardObj.gameboard.length; i++){
             let content = document.createElement("div")
@@ -15,18 +16,52 @@ const gameBoard = (function() {
             content.style.height = "100px"
             content.classList.add("square")
             gameContent.append(content) 
-            content.addEventListener("click", ()=>{
-                if(content.innerText == "x"){
-                    content.innerText= "o"
-                }else if(content.innerText == "o"){
+            content.addEventListener("click", () =>{
+                if(counter % 2 == 0 && gameBoardObj.gameboard[i] == ""){
+                    gameBoardObj.gameboard[i] = "x"
                     content.innerText = "x"
-                }            
+                    counter++
+                }else if(counter % 2 == 1 && gameBoardObj.gameboard[i] == ""){
+                    content.innerText = "o"
+                    gameBoardObj.gameboard[i] = "o"
+                    counter++
+                }if(counter == 9){
+                    console.log(gameBoardObj)
+                    checkWin()
+                }
             })
+
+
+  
+
+
         }
-    }
+
+        }
+
+        function checkWin(){
+                if(gameBoardObj.gameboard[0] == "x" && gameBoardObj.gameboard[1] == "x" && gameBoardObj.gameboard[2] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[3] == "x" && gameBoardObj.gameboard[4] == "x" && gameBoardObj.gameboard[5] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[6] == "x" && gameBoardObj.gameboard[7] == "x" && gameBoardObj.gameboard[8] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[0] == "x" && gameBoardObj.gameboard[3] == "x" && gameBoardObj.gameboard[6] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[1] == "x" && gameBoardObj.gameboard[4] == "x" && gameBoardObj.gameboard[7] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[2] == "x" && gameBoardObj.gameboard[5] == "x" && gameBoardObj.gameboard[8] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[0] == "x" && gameBoardObj.gameboard[4] == "x" && gameBoardObj.gameboard[8] == "x"){
+                    console.log("x wins")
+                }else if(gameBoardObj.gameboard[2] == "x" && gameBoardObj.gameboard[4] == "x" && gameBoardObj.gameboard[6] == "x"){
+                    console.log("x wins")
+                }
+        
+
+
 
     createGame()
-
     
 
 
